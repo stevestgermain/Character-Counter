75cdd399e63a7dd69f68be3fda97ce50f7956111
stevestgermain <stevesaintgermain@gmail.com>
2025-12-29 16:52:03 -0500

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class', // This enables dark mode via class on html element
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
