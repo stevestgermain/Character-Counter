9c26c435a738581143459103615c83ff674fb100
stevestgermain <stevesaintgermain@gmail.com>
2025-12-29 18:02:18 -0500

    Change CSS import path to root directory

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './src/index.css';  // ADD THIS LINE
+import './index.css';  // Changed to import from root
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
